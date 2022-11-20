7f95b77b64e7d52a1a8877ab3350031c6884499d
hutamatr <hutamatr@gmail.com>
2022-11-21 01:07:49 +0700

    changes styles in some components

--- src/components/AdvancedStatistic/AdvancedStatistic.tsx
+++ src/components/AdvancedStatistic/AdvancedStatistic.tsx
@@ -4,12 +4,12 @@ import StatisticCard from "./StatisticCard";
 
 const AdvancedStatistic = () => {
   return (
-    <section className="relative bottom-6 mb-8 flex flex-col gap-y-12 px-5 text-center">
-      <div className="flex flex-col gap-y-4">
+    <section className="relative bottom-6 mb-8 mt-10 flex flex-col gap-y-12 px-5 text-center md:mb-24">
+      <div className="mb-12 flex flex-col gap-y-4 sm:mb-16">
         <h2 className="text-2xl font-bold text-primary-dark-violet">
           Advanced Statistic
         </h2>
-        <p className="text-sm font-medium text-neutral-grayish-violet">
+        <p className="text-sm font-medium text-neutral-grayish-violet md:mx-auto md:max-w-md">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
